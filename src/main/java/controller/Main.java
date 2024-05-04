7442195cf05202218acf4e7c7ac2b045a9d6a8da
Bui-Minh-Quan <buiminhquandz2005@gmail.com>
2024-05-04 09:14:12 +0700

    redesign hangman game

---
+++ src/main/java/controller/Main.java
@@ -0,0 +1,32 @@
+package controller;
+
+import database.Database;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import logic.Word;
+
+import java.sql.SQLException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/HangmanGame/HangmanGameController.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Hangman Game");
+        stage.show();
+
+    }
+
+}
+
