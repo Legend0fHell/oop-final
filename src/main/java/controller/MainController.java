c60474d686a49d04dcf91292158c59d6a2e78b1c
Bui-Minh-Quan <buiminhquandz2005@gmail.com>
2024-05-08 21:41:44 +0700

    adding icon images for quiz game, hangman and dictionary.

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class MainController extends Application {
     public static void main(String[] args) {
@@ -13,9 +16,13 @@ public class MainController extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Image image = new Image(Objects.requireNonNull(getClass()
+                .getResourceAsStream("/Dictionary/icon.png")));
+
         Parent root = FXMLLoader.load(getClass().getResource("/Dictionary/DictionaryInterface.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.getIcons().add(image);
         stage.setTitle("English Learning Application");
         stage.show();
     }
